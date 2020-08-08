3e24e5d141dd452690245be19fb9105d5f2f68c7
Khanh Christopher Tran <khanh.christopher.tran@gmail.clm>
2020-08-07 21:42:00 -0700

    new build run

---
+++ precache-manifest.65d22d7cadcfa7d097985b70cbf21fd7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e39c20a7e9468ba21f87f2d96b13645",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c3a2402278cdc978f79",
+    "url": "/static/css/main.a9975bf5.chunk.css"
+  },
+  {
+    "revision": "30df39af1e864c161154",
+    "url": "/static/js/2.4821ae6d.chunk.js"
+  },
+  {
+    "revision": "56b6475755c4137f98de2c204c815aa8",
+    "url": "/static/js/2.4821ae6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c3a2402278cdc978f79",
+    "url": "/static/js/main.f736680d.chunk.js"
+  },
+  {
+    "revision": "c2f98a5c387da539a513",
+    "url": "/static/js/runtime-main.5e682c5b.js"
+  }
+]);
